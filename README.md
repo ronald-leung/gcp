22a64b19205596ac1e750f63c729543b69c2bf49
Ronald Leung <ceo@smileyapps.com>
2020-04-26 17:19:04 -0700

    Add python streaming job for pub sub streaming dataexample

--- README.md
+++ README.md
@@ -7,6 +7,20 @@ Here are the examples scenarios:
    - This is a very common use cases. Some application has some data, that is generated periodically. 
    We want to visualize it, do machine learning, analysis, yadi yadi yadda. This pipeline allows you to
    get generic data to the right place. Once data is in Bigquery then we are free to use it however we like. 
+   - Resources to setup before building this example:
+      - Pub Sub Topic
+      - Pub Sub Subscription
+      - Google Cloud Storage Bucket
+      - Big Query Table
+         - To create the table, run 
+            bq mk --table project_id:dataset.table schema.json
+            
+   - Components that we will be building:
+      - App engine, to continuously generate test data, and inserting data into Google Cloud Storage
+      - Google cloud function that gets triggered every time a new file gets inserted to Google Cloud Storage. 
+      It'll read the file, and publish the data to a Pub Sub Topic
+      - Google streaming dataflow job, that will read the data from a pub sub subscription, and stores the data into a BigQuery table.
+        
    
 - Cloud composer
    - QA automation tests
---
+++ dataflow/pubsub_to_bigquery.py
@@ -0,0 +1,90 @@
+"""
+This dataflow job will insert data from a Pub Sub topic into a Bigquery table
+
+Input:
+- Pubsub subscription name
+- Big query table name
+
+Output:
+- Bigquery dataset:tablename
+
+"""
+
+import argparse
+import logging
+import json
+import apache_beam as beam
+from apache_beam.io import ReadFromText
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.io import BigQueryDisposition
+
+import pandas as pd
+import re
+
+
+# {"eventtime":"2020-04-26 00:00", "eventtext":"abc", "eventint":"123"}
+class StreamExampleDoFn(beam.DoFn):
+    def __init(self):
+        beam.DoFn.__init__(self)
+
+    def pubsubJsonTransform(self, messageContent):
+        print(messageContent)
+        return [json.loads(messageContent)]
+
+    def process(self, element):
+        """ Utility function to do any additional processing before writing the message into big query
+        """
+
+        return self.pubsubJsonTransform(element)
+
+
+class StreamDataOptions(PipelineOptions):
+    @classmethod
+    def _add_argparse_args(cls, parser):
+        parser.add_argument(
+            '--dataSubscription',
+            # required=True,
+            default='projects/dbk-login-log-anlysis-wug01-qa/subscriptions/streaming_example_sub',
+            help='Pub subscription name')
+        parser.add_argument(
+            '--bigQueryTableRef',
+            # required=True,
+            default='dbk-login-log-anlysis-wug01-qa:streaming_dataflow_example.streaming_example',
+            help='Target Big Query table name')
+
+
+''' 
+    Command line options sample values for:
+        Running directly:
+        '--runner=DirectRunner',
+    
+        Running in Google Dataflow:
+        '--runner=DataflowRunner',    
+        '--project=dbk-login-log-anlysis-wug01-qa',
+        '--staging_location=gs://dbk-login-log-anlysis-dataflow-qa/staging',
+        '--temp_location=gs://dbk-login-log-anlysis-dataflow-qa/temp',
+        '--job_name=stream_pubsub_example',
+        
+        For generating template
+        '--template_location=gs://dbk-login-log-anlysis-dataflow-qa/templates/stream_pubsub_example_template'
+'''
+
+def run(argv=None, save_main_session=True):
+    parser = argparse.ArgumentParser()
+    known_args, pipeline_args = parser.parse_known_args(argv)
+    pipeline_options = PipelineOptions(pipeline_args)
+    pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+    p = beam.Pipeline(options=pipeline_options)
+    user_options = pipeline_options.view_as(StreamDataOptions)
+    print(user_options)
+    lines = p | 'Read From Pub sub' >> beam.io.ReadFromPubSub(subscription=user_options.dataSubscription).with_output_types(bytes)
+    decode = lines | "Decode" >> beam.Map(lambda x: x.decode('utf-8'))
+    jsonObj = decode | 'Get JSON' >> (beam.ParDo(StreamExampleDoFn()))
+    jsonObj | 'Write to Big Query' >> beam.io.WriteToBigQuery(user_options.bigQueryTableRef, write_disposition=BigQueryDisposition.WRITE_APPEND)
+    result = p.run()
+    result.wait_until_finish()
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel(logging.INFO)
+    run()
